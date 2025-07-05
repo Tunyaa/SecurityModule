6259b73215bc5007b6c332c2d124cfd0ef98838c
tunyaa <ildcampthunder@gmail.com>
2025-07-06 02:11:33 +0300

    OK. Completed security module. Authentication \ 3 levels of roles

--- src/main/java/com/module/securityModule/config/SecurityConfig.java
+++ src/main/java/com/module/securityModule/config/SecurityConfig.java
@@ -1,9 +1,17 @@
 package com.module.securityModule.config;
 
+import com.module.securityModule.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 
 /**
@@ -25,8 +33,26 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
-                .loginPage("/login").permitAll()
-                );
+                .loginPage("/login")
+                .permitAll()
+                .defaultSuccessUrl("/", true)
+                )
+                .logout(logout -> logout.permitAll());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+
+        User user = new User("user", passwordEncoder.encode("user123"), "ROLE_USER");
+        User organizer = new User("organizer", passwordEncoder.encode("organizer123"), "ROLE_ORGANIZER");
+        User admin = new User("admin", passwordEncoder.encode("admin123"), "ROLE_ADMIN");
+
+        return new InMemoryUserDetailsManager(user, organizer, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/module/securityModule/model/User.java
+++ src/main/java/com/module/securityModule/model/User.java
@@ -1,18 +1,51 @@
 package com.module.securityModule.model;
 
-
+import java.util.Collection;
+import java.util.Collections;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 /**
  *
  * @author tunyaa
  */
-//@Data
-//@NoArgsConstructor
-public class User {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
 
     private String username;
     private String password;
-    private String email;
-    private String role = "ROLE_USER"; // ROLE_USER \ ROLE_ORGANIZER \ ROLE_ADMIN
+//    private String email;
+    private String role = "USER"; // ROLE_USER \ ROLE_ORGANIZER \ ROLE_ADMIN
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 }
